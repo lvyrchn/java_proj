90cb3fa5ec7a8fe8332dc36acc2883fee1319a84
lvyrchn <55714487+lvyrchn@users.noreply.github.com>
2019-09-24 01:21:51 -0500

    Update BumpingBalls.java

--- BumpingBalls.java
+++ BumpingBalls.java
@@ -1,3 +1,5 @@
+// Create bumping balls that bounce back when colliding with each other or the boundary
+
 package lab4;
 
 import java.awt.Color;
@@ -10,7 +12,6 @@ public class BumpingBalls {
 	public static void main(String[] args) {
 		ArgsProcessor ap = new ArgsProcessor(args);
 		int N = ap.nextInt("Enter the number of balls: ");
-		//int I = ap.nextInt("Enter the number of iterations: ");
 
 		StdDraw.setXscale(-1.0, 1.0);
 		StdDraw.setYscale(-1.0, 1.0);
@@ -22,8 +23,6 @@ public class BumpingBalls {
 		for (int i = 0 ; i < N ; i = i + 1) {
 			position[i][0] = (2*Math.random()-1);
 			position[i][1] = (2*Math.random()-1);
-			//StdDraw.setPenColor(Color.PINK);
-			//StdDraw.filledCircle(position[i][0],position[i][1], r);
 		}
 
 		double[][] velocity = new double[N][2];
@@ -33,11 +32,7 @@ public class BumpingBalls {
 			velocity[i][1] = 0.025;
 		}
 
-
 		double[][] distance = new double[N][N];
-
-
-
 
 		for (int i = 0 ; i < N ; i = i + 1) {
 
@@ -45,8 +40,6 @@ public class BumpingBalls {
 
 			}
 		}
-
-
 
 		while (true) {
 			StdDraw.clear();
@@ -73,9 +66,6 @@ public class BumpingBalls {
 						}}
 					}
 				}
-			
-				
-		
 
 				position[i][0] = position[i][0] + velocity[i][0];		
 				position[i][1] = position[i][1] + velocity[i][1];
@@ -85,76 +75,9 @@ public class BumpingBalls {
 
 			}
 			StdDraw.show(20); 
-
-
-			//StdDraw.clear();
-
-			//	StdDraw.setPenColor(StdDraw.GRAY);
-			//	StdDraw.filledSquare(0, 0, 1.0);
-
-
-
+			
 		}
 
-
-
-
-		/*
-		double rx1 = 2*Math.random()-1;
-		double ry1 = 2*Math.random()-1;
-
-		double rx2 = 2*Math.random()-1;
-		double ry2 = 2*Math.random()-1;
-
-		double vx1 = 0.1;
-		double vy1 = 0.1;
-
-		double vx2 = 0.05;
-		double vy2 = 0.05;
-
-
-		while (true) {
-
-			double dx = Math.abs(rx1-rx2);
-			double dy = Math.abs(ry1-ry2);
-
-
-			if (Math.abs(rx1 + vx1) > 1.0 - r || (dx <= 2*r && dy <= 2*r ) ) {
-				vx1 = -vx1;
-			}
-			if (Math.abs(ry1 + vy1) > 1.0 - r || (dx <= 2*r && dy <= 2*r )) {
-				vy1 = -vy1;
-			}
-
-			if (Math.abs(rx2 + vx2) > 1.0 - r || (dx <= 2*r && dy <= 2*r)) {
-				vx2 = -vx2;
-			}
-			if (Math.abs(ry2 + vy2) > 1.0 - r || (dx <= 2*r && dy <= 2*r)) {
-				vy2 = -vy2;
-			}
-
-
-			// update position
-			rx1 = rx1 + vx1; 
-			ry1 = ry1 + vy1; 
-
-
-			rx2 = rx2 + vx2; 
-			ry2 = ry2 + vy2; 
-
-
-			StdDraw.clear();
-
-			StdDraw.setPenColor(Color.PINK);
-			StdDraw.filledCircle(rx1,ry1, r);
-			StdDraw.filledCircle(rx2,ry2, r);
-			StdDraw.show(200); 
-
-
-			//double[] histX = new double[N];
-			//double[] histY = new double[N];
-		}
-		 */
 	}
 }
 
